1b0b61e47beb0b2306449ee39122f6f151ee0bbf
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2020-07-06 20:13:36 -0400

    new design

--- navbar.js
+++ navbar.js
@@ -10,6 +10,7 @@ if ($(window).width() > 600 && $(this).scrollTop() < 100) {
 
 function toHome() {
     $(".topnav a").css("background-color", "transparent");
+    $(".topnav a").css("color", "white");
 }
 
 function myFunction() {
@@ -22,14 +23,16 @@ function myFunction() {
 }
 
 function transNav() {
-    topNav.style.backgroundColor = "transparent";
+    $(".topnav a").css("color", "white");
+    topNav.style.backgroundColor = "black";
     topNav.style.border = "none";
     $(".topnav a").hover(function () {
-        $(this).css("background-color", "transparent");
-        $(this).css("color", "white");
+        $(".topnav a").css("color", "white");
+        $(this).css("background-color", "black");
+        $(this).css("color", "rgb(74, 74, 128)");
     });
     $(".topnav a").mouseout(function () {
-        $(this).css("color", "black");
+        $(this).css("color", "white");
     });
 }
 
@@ -61,6 +64,24 @@ $(window).on('scroll', function () {
             whiteNav();
         } else {
             transNav();
+            $(".topnav a").css("background-color", "transparent");
         }
     }
+});
+
+$(document).ready(function(){
+	var my_posts = $("[rel=tooltip]");
+
+	var size = $(window).width();
+	for(i=0;i<my_posts.length;i++){
+		the_post = $(my_posts[i]);
+
+		if(the_post.hasClass('invert') && size >=767 ){
+			the_post.tooltip({ placement: 'left'});
+			the_post.css("cursor","pointer");
+		}else{
+			the_post.tooltip({ placement: 'rigth'});
+			the_post.css("cursor","pointer");
+		}
+	}
 });
